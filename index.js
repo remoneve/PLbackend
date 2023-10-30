7b153e4876853084fe3a5a6840d4accf649b941b
remoneve <veeti.p.remonen@student.jyu.fi>
2023-10-30 16:51:56 +0200

    added frontend func

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
+
 const app = express()
 
 morgan.token('type', function(req) { 
@@ -8,6 +11,8 @@ morgan.token('type', function(req) {
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
@@ -106,6 +111,7 @@ let persons = [
     res.status(204).end()
   })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.port || 3001
+app.listen(PORT, () =>  {
+  console.log(`Server running on port ${PORT}`)
+})
